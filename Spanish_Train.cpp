8a6251fc59d572d4b4d921b341fa71d17ede7e02
mostafamoknaa <mostafamokna78@gmail.com>
2024-12-12 20:21:44 +0200

    Spanish Train

--- Spanish_Train.cpp
+++ Spanish_Train.cpp
@@ -1,1 +1,40 @@
 #include "Spanish_Train.h"
+#include "Train.h"
+#include "Coach.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Spanish_Train::Spanish_Train(int Num, string Type, int Max) {
+	this->T_Num = Num;
+	this->Type = Type;
+	this->Max_Ticket = Max;
+}
+
+void Spanish_Train::Show_Train_Details() {
+	Train::Show_Train_Details();
+	cout << "Maximum Ticket: " << Max_Ticket << endl;
+}
+
+double Spanish_Train::CalculateTotalRevenue() {
+	double Total = 0.0;
+	for (Coach coach : Coaches) {
+		Total += coach.C_Price*(coach.Seets - coach.Avaiable_Steets);
+	}
+	return Total;
+}
+
+void Spanish_Train::SearchByCoachType(string Type) {
+	bool Check = 0;
+	for (Coach coach : Coaches) {
+		if (coach.Type == Type) {
+			coach.Show_Coach_Details();
+			Check = 1;
+		}
+	}
+	if (!Check) {
+		cout << "No Coaches of type in that Train'" << Type << "' found." << endl;
+	}
+}
+
--- Spanish_Train.h
+++ Spanish_Train.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "Train.h"
+#include <string>
+using namespace std;
+
 class Spanish_Train : public Train{
+private:
+	int Max_Ticket;
+public:
+	Spanish_Train(int, string, int);
+	void Show_Train_Details();
+	double CalculateTotalRevenue();
+	void SearchByCoachType(string type);
 
 };
 
